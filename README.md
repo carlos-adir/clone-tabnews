9608ce13187d0dcf61a7ba62af10bc7bfc9f6201
Carlos Adir <carlos.adir.leite@gmail.com>
2023-12-11 19:26:37 +0000

    Adiciona configurações de formatação e estilização

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Um clone do https://tabnews.com.br para o curso https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-    return <h1>Oi Dezinha, se você me ama, dá uma risadinha 😎</h1>
+function Home() {
+  return <h1>Oi Dezinha, se você me ama, dá uma risadinha 😎</h1>;
 }
-
 
 export default Home;
